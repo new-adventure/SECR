07849bef091ff7e1b4b2499e50404e9e7f9bd9cf
Tarek Ali <tarekali15@outlook.com>
2021-09-18 10:46:19 -0700

    removed unnecessary scripts

--- nmap_scripts/nmap_all_ports/nmap_allports_scan.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-nmap -oX "$1_allports.xml" -p 1-65535  $1 > tmpAP
-cp tmpAP allports_scan_output.txt
-grep /tcp tmpAP > portinfo.ap.txt
-sed -i 's/\/tcp.*//' portinfo.ap.txt
-rm tmpAP
