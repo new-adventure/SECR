a92637470415b3a51158cb5dda66b818f43e3fb6
Tarek Ali <tarekali15@outlook.com>
2021-09-18 07:59:44 -0700

    working on state management for checkbox

--- secr/src/components/form/Form.js
+++ secr/src/components/form/Form.js
@@ -1,25 +1,28 @@
 import { useState } from 'react';
 
 function Form() {
-    let [url, setUrl] = useState('localhost:8080');
-    console.log(url);
+    const [url, setUrl] = useState('localhost:8080');
+    const [sqlInjection, setSqlInjection] = useState(true);
+    const [ddos, setDdos] = useState(false);
+    const [scanning, setScanning] = useState(false);
+    console.log(sqlInjection, ddos, scanning);
     return (
         <form>
             <div class="main-input">
-                <input class="url" type="text" placeholder="Enter URL..." onChange={i => setUrl(i.target.value)} />
+                <input class="url" type="text" placeholder="Enter URL..." />
                 <input class="submit" type="submit" value="Check!" />
             </div>
             <div class="checkbox-buttons">
                 <div class="checkbox-wrapper">
-                    <input type="checkbox" name="type" id="c-1" value="SQL Injection" />
+                    <input type="checkbox" name="type" id="c-1" value="SQL Injection" checked={sqlInjection} />
                     <label class="checkbox-label" for="c-1">SQL Injection</label>
                 </div>
                 <div class="checkbox-wrapper">
-                    <input type="checkbox" name="type" id="c-2" value="DDOS" />
+                    <input type="checkbox" name="type" id="c-2" value="DDOS" checked={ddos} />
                     <label class="checkbox-label" for="c-2">DDOS</label>
                 </div>
                 <div class="checkbox-wrapper">
-                    <input type="checkbox" name="type" id="c-3" value="Port Scanning" />
+                    <input type="checkbox" name="type" id="c-3" value="Port Scanning" checked={scanning} />
                     <label class="checkbox-label" for="c-3">Port Scanning</label>
                 </div>
             </div>
