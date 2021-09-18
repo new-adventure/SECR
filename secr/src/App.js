ae0b8c7bfbf0b07e8557b6c74cf4e93b3fc8d21c
Shubh Sheth <shubhsheth8@gmail.com>
2021-09-18 02:10:22 -0500

    form updates

--- secr/src/App.js
+++ secr/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 import Header from './components/header/Header';
+import Form from './components/form/Form';
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
+			<Form />
 		</div>
 	);
 }
---
+++ secr/src/components/form/Form.js
@@ -0,0 +1,26 @@
+function Form() {
+	return (
+		<form>
+            <div class="main-input">
+                <input class="url" type="text" placeholder="Enter URL..." />
+                <input class="submit" type="submit" value="Check!" />
+            </div>
+            <div class="checkbox-buttons">
+                <div class="checkbox-wrapper">
+                    <input type="checkbox" name="type" id="c-1" value="SQL Injection" />
+                    <label class="checkbox-label" for="c-1">SQL Injection</label>
+                </div>
+                <div class="checkbox-wrapper">
+                    <input type="checkbox" name="type" id="c-2" value="DDOS" />
+                    <label class="checkbox-label" for="c-2">DDOS</label>
+                </div>
+                <div class="checkbox-wrapper">
+                    <input type="checkbox" name="type" id="c-3" value="Port Scanning" />
+                    <label class="checkbox-label" for="c-3">Port Scanning</label>
+                </div>
+            </div>
+        </form>
+	);
+}
+
+export default Form;
