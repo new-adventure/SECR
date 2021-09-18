af093c0d1839ce6d0ef78c91a3403b012cf9e1e4
Tarek Ali <tarekali15@outlook.com>
2021-09-18 11:18:56 -0700

    work in progress

--- api/nmap_scripts/nmap_sV/nmap_sV_scan.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo 'PORT    STATE    SERVICE VERSION' > serviceinfo
-sudo nmap -oX "$1_sV.xml" -sV -T5 $1 > tmpsV
-cp tmpsV sV_scan_output.txt
-grep /tcp tmpsV >> serviceinfo.txt
-rm tmpsV
