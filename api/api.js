43abc4188ee98cc096a6472e0fa8383e1c80c97e
Tarek Ali <tarekali15@outlook.com>
2021-09-18 15:07:55 -0700

    changed script execution

--- api/api.js
+++ api/api.js
@@ -15,7 +15,7 @@ app.get('/api/scan', (req, res) => {
 });
 
 const executeScan = (url) => {
-    exec(`sh nmap_vuln/nmap_vuln_scan ${url}`, (error, stdout, stderr) => {
+    exec(`sh nmap_vuln/nmap_ports ${url}`, (error, stdout, stderr) => {
         if (error) {
             console.error(err);
         }
