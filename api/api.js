f0d60259550c1be0240bffebda0c32ff81c90686
Shubh Sheth <shubhsheth8@gmail.com>
2021-09-18 21:09:47 -0500

    Revert "output for vulnscan"
    This reverts commit 9bd22d64ffd27fe6b8991a472b3230987d8e5d1a.

--- api/api.js
+++ api/api.js
@@ -1,1 +1,28 @@
-localStorage
+const { exec } = require('child_process');
+const { application } = require('express');
+const express = require('express');
+const { fileURLToPath } = require('url');
+const app = express();
+
+app.get('/api/scan', (req, res) => {
+    /* 
+        Desired interface:
+        - /api/scan?url=<url> 
+        Response:
+        - CLI output of nmap vuln scan and parsing accordingly 
+    */
+    const output = executeScan(req.query.url);
+    res.send("hello world");
+});
+
+const executeScan = (url) => {
+    exec(`sh nmap_vuln/nmap_ports ${url}`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(err);
+        }
+        console.log(stdout)
+        return stdout;
+    });
+}
+
+app.listen(8080);
