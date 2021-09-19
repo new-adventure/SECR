3821e9d2d604b421a4f29849a188d1370aba3b84
Shubh Sheth <shubhsheth8@gmail.com>
2021-09-18 19:07:20 -0500

    credit links

--- secr/src/components/credits/Credits.js
+++ secr/src/components/credits/Credits.js
@@ -5,8 +5,8 @@ function Credits() {
                 Built by &nbsp;
                 <a href="https://github.com/shubhsheth" target="_blank">Shubh Sheth</a>,&nbsp;
                 <a href="https://github.com/DGKSK8LIFE" target="_blank">Tarek Ali</a>,&nbsp;  
-                <a href="https://github.com/jemmemy" target="_blank">Jeremy He</a>,
-                Sebastian 
+                <a href="https://github.com/jemmemy" target="_blank">Jeremy He</a>,&nbsp;
+                <a href="https://github.com/thwim" target="_blank">Sebastian</a>
             </p>
             <p><a href="https://hack.rice.edu/" target="_blank">HackRice 11 Project</a></p>
         </div>
