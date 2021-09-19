e74dce086d12dad8bd5d3c08b1d2b6b28f049639
jemmemy <jeremy@goldnex.com>
2021-09-18 21:39:51 -0500

    Middleware fix

--- api/api.js
+++ api/api.js
@@ -4,25 +4,39 @@ const express = require('express');
 const { fileURLToPath } = require('url');
 const app = express();
 
-app.get('/api/scan', (req, res) => {
+app.get('/api/scan', async (req, res) => {
     /* 
         Desired interface:
         - /api/scan?url=<url> 
         Response:
         - CLI output of nmap vuln scan and parsing accordingly 
     */
-    const output = executeScan(req.query.url);
-    res.send("hello world");
+    const output = await executeScan(req.query.url);
+    console.log(output)
+    res.send(output);
 });
 
 const executeScan = (url) => {
-    exec(`sh nmap_vuln/nmap_ports ${url}`, (error, stdout, stderr) => {
+    /*   exec(`sh nmap_vuln/nmap_ports ${url}`, (error, stdout, stderr) => {
         if (error) {
-            console.error(err);
+            console.error(error);
         }
         console.log(stdout)
         return stdout;
-    });
+    });   */
+    return execShellCommand(`sh nmap_vuln/nmap_ports ${url}`)
 }
+
+function execShellCommand(cmd) {
+    const exec = require('child_process').exec;
+    return new Promise((resolve, reject) => {
+     exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+       console.warn(error);
+      }
+      resolve(stdout? stdout : stderr);
+     });
+    });
+   }
 
 app.listen(8080);
