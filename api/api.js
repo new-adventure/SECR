1cfd50e4ad201a1e15a0946df183e2db9c1b7b2d
Tarek Ali <tarekali15@outlook.com>
2021-09-18 22:19:03 -0700

    proper formatting

--- api/api.js
+++ api/api.js
@@ -30,13 +30,13 @@ const executeScan = (url) => {
 function execShellCommand(cmd) {
     const exec = require('child_process').exec;
     return new Promise((resolve, reject) => {
-     exec(cmd, (error, stdout, stderr) => {
-      if (error) {
-       console.warn(error);
-      }
-      resolve(stdout? stdout : stderr);
-     });
+        exec(cmd, (error, stdout, stderr) => {
+            if (error) {
+                console.warn(error);
+            }
+            resolve(stdout ? stdout : stderr);
+        });
     });
-   }
+}
 
 app.listen(8080);
